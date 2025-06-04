e84aea4d729e98297a4f9100e419aa6b025d8eb8
roblesdotdev <httpsnip@gmail.com>
2025-06-04 18:40:12 -0300

    initialize build logic module

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.roblesdotdev.habits.buildlogic"
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.ksp.gradlePlugin)
+    compileOnly(libs.room.gradlePlugin)
+}
