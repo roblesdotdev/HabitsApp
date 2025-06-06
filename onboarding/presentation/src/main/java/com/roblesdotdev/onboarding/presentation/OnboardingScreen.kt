f20cbc8a4795483b9becddb93a537c142d6a6056
Aldo R. Robles <90198035+roblesdotdev@users.noreply.github.com>
2025-06-06 20:38:51 -0300

    core: add color scheme (#9)

--- onboarding/presentation/src/main/java/com/roblesdotdev/onboarding/presentation/OnboardingScreen.kt
+++ onboarding/presentation/src/main/java/com/roblesdotdev/onboarding/presentation/OnboardingScreen.kt
@@ -1,20 +1,28 @@
 package com.roblesdotdev.onboarding.presentation
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.roblesdotdev.core.presentation.designsystem.success
 
 @Composable
 fun OnboardingScreen(
     onCompleteOnboarding: () -> Unit
 ) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background),
+        contentAlignment = Alignment.Center
+    ) {
         TextButton(onClick = onCompleteOnboarding) {
-            Text("Get started")
+            Text("Get started", color = MaterialTheme.colorScheme.success)
         }
     }
 }
