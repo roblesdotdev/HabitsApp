1eb5422b65ae5783a76d0eddee39c7c1fc2b871d
roblesdotdev <httpsnip@gmail.com>
2025-06-09 18:18:41 -0300

    add error wrapper utility

---
+++ core/domain/src/main/java/com/roblesdotdev/core/domain/util/Result.kt
@@ -0,0 +1,19 @@
+package com.roblesdotdev.core.domain.util
+
+sealed interface Result<out D, out E: Error> {
+    data class Success<out D>(val data: D) : Result<D, Nothing>
+    data class Failure<out E: Error>(val error: E): Result<Nothing, E>
+}
+
+inline fun<T, E: Error, R> Result<T, E>.map(map: (T) -> R): Result<R, E> {
+    return when(this) {
+        is Result.Failure -> Result.Failure(error)
+        is Result.Success -> Result.Success(map(data))
+    }
+}
+
+fun <T, E: Error> Result<T, E>.asEmptyResult(): EmptyResult<E> {
+    return map {}
+}
+
+typealias EmptyResult<E> = Result<Unit, E>
