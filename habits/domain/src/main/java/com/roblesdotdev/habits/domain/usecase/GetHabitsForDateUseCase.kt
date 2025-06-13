f45db499eac0f95ebfde457bac3b84bc7a89f02a
roblesdotdev <httpsnip@gmail.com>
2025-06-13 00:14:44 -0300

    refactor: filter habits peer date & update model

--- habits/domain/src/main/java/com/roblesdotdev/habits/domain/usecase/GetHabitsForDateUseCase.kt
+++ habits/domain/src/main/java/com/roblesdotdev/habits/domain/usecase/GetHabitsForDateUseCase.kt
@@ -3,11 +3,16 @@ package com.roblesdotdev.habits.domain.usecase
 import com.roblesdotdev.core.domain.habit.Habit
 import com.roblesdotdev.habits.domain.HabitsRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import java.time.ZonedDateTime
 
 class GetHabitsForDateUseCase(
     private val repo: HabitsRepository
 ) {
-    operator fun invoke(date: Long): Flow<List<Habit>> {
-       return repo.getHabitsForSelectedDate(date)
+    operator fun invoke(date: ZonedDateTime): Flow<List<Habit>> {
+        return repo.getHabitsForSelectedDate(date).map { habit ->
+            habit.filter { it.frequency.contains(date.dayOfWeek) }
+        }.distinctUntilChanged()
     }
 }
